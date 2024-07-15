3b4bf56ec40b9c0ef9e17d1b88fdb5c3b84f37b8
Vikash Jain <141486717+VikashJain2@users.noreply.github.com>
2024-07-15 15:07:03 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://yumyard-backend-xll3.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
   const [menu_list,setMenuList] = useState([])
